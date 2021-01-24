e8aaf4bf8c3ba1ac4fa735968c7f7eaa23fbb4f1
Guillermo <gjsuarezruiz@gmail.com>
2021-01-24 20:10:13 +0100

    creado vista

---
+++ app/src/main/java/com/guillermo/cine/login/view/loginView.java
@@ -0,0 +1,16 @@
+package com.guillermo.cine.login.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.guillermo.cine.R;
+
+public class loginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_view);
+    }
+}
